2baaa5e8a2c0d8138d292c1436d1fe13c7508916
Hemant Kawade <hmntkwd@gmail.com>
2026-03-19 16:02:35 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,12 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/products", () =>
-{
-    return new[] {
-        new { Id = 1, Name = "Laptop" },
-        new { Id = 2, Name = "Phone" }
-    };
-});
+app.MapGet("/", () => "Hello World!");
 
 app.Run();
